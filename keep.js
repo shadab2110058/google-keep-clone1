7da208fb670d3c983672739482799bca34a9d9af
shadab2110058 <113245069+shadab2110058@users.noreply.github.com>
2022-10-28 18:27:51 +0530

    Update keep.js

--- keep.js
+++ keep.js
@@ -2,7 +2,7 @@
 const addButton = document.getElementById("addNote");
 const updateLSData = ()=>{
    const textAreaData = document.querySelectorAll('textarea');
-   console.log(textAreaData);
+   //console.log(textAreaData);
    const not = [];
     
    textAreaData.forEach((i)=>{
@@ -36,18 +36,17 @@ const updateLSData = ()=>{
             deleteButton.addEventListener('click',  () =>
                {
                  i.remove();
+                updateLSData();   
                 });
               
-                  // textArea.value = text;
-                  // // bigDivText.value = text;
-                  // mainButton.innerHTML = text;
+                   textArea.value = text;
+                   mainButton.innerHTML = text;
 
                   editButton.addEventListener('click',() =>{ 
                   mainButton.classList.toggle('hidden');
                   textArea.classList.toggle('hidden');
             })
-               // textArea.addEventListener('change',(event)=>
-               // bigDivText.addEventListener('change',(event)=>
+      
                textArea.addEventListener('change',(event)=>
                {
                   const value = event.target.value;
@@ -57,19 +56,11 @@ const updateLSData = ()=>{
                   updateLSData();    
                });
 
-               // textArea.addEventListener('change',(event)=>
-               // {
-               //    const value = event.target.value;
-               //    textArea.innerHTML = value;
-               //    // bigDivText.innerHTML = value;
-               //    mainButton.innerHTML = value;   
-               //    updateLSData();    
-               // });
+            
 
 
                document.body.appendChild(i);
-               // addButton.addEventListener('click',()=>addNewNote());
-
+              
    }
                const not = JSON.parse(localStorage.getItem('not'));
                if(not)
